ace200b760e6cc34a1aca302fdfdea2df407840f
SauloJCF <55318912+SauloJCF@users.noreply.github.com>
2021-02-10 19:55:05 -0300

    Add project files.

---
+++ cvrp-project/Entities/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cvrp_project.Entities
+{
+    public class Point
+    {
+        public int Id { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Demand { get; set; }
+
+        private double GetDistance(Point p2)
+        {
+            return Math.Sqrt(Math.Pow(p2.X - X, 2) + Math.Pow(p2.Y - Y, 2));
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}; X: {X}; Y: {Y}; Demand: {Demand};";
+        }
+    }
+}
